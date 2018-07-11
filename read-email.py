3298a8cddf456efabda92a308d0b1a853c4c3e8f
Duvanchi <cochize0@gmail.com>
2018-07-11 11:38:24 -0400

    Fine tuned special character removal

--- read-email.py
+++ read-email.py
@@ -4,6 +4,7 @@ import email
 import secrets
 import os
 import csv
+import re
 
 parsedData = []
 tweets = []
@@ -20,21 +21,28 @@ def login_and_write():
     # Searches inbox for emails from desired address
     messages = server.search(['FROM', secrets.target_email])
 
-    info_sheet = open('//CHFS/Shared Documents/OpenData/datasets/staging/fire-dept-twitter.csv', 'w')    
+    # opens csv
+    info_sheet = open('fire-dept-twitter.csv', 'w')    
 
-    if os.stat('//CHFS/Shared Documents/OpenData/datasets/staging/fire-dept-twitter.csv').st_size == 0:
-        info_sheet.write("Address, City, Type of Incident \n")
+    # if there's no header, write a header
+    if os.stat('fire-dept-twitter.csv').st_size == 0:
+        info_sheet.write("Address, City, Type of Incident \n") 
 
     # loops through messages to fetch text content
     # reassigns the parsedData list each iteration to prevent duplicates
     for msgid, data in server.fetch(messages, ['BODY[TEXT]']).items():
         envelope = data[b'BODY[TEXT]']
-        target = str(envelope.decode()[14:67])
+        target = str(envelope.decode()[14:])
         parsedData = target.split(";")
         parsedData.pop(0)
-        info_sheet.write(parsedData[0] + ",")
-        info_sheet.write(parsedData[1] + ",")
-        info_sheet.write(parsedData[2] + "\n")
+        # The emails typically contain special characters
+        # using regex to remove them
+        new_string1 = re.sub('[^A-Za-z0-9 ]+', '', parsedData[0])
+        new_string2 = re.sub('[^A-Za-z0-9 ]+', '', parsedData[1])
+        new_string3 = re.sub('[^A-Za-z0-9 ]+', '', parsedData[2])
+        info_sheet.write(new_string1 + ",")
+        info_sheet.write(new_string2 + ",")
+        info_sheet.write(new_string3 + "\n")
 
     logout(server)
 
