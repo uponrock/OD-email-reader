7aeefd71411587723bf9d605fbbd4775e5daa339
emhoo <emhoo@live.unc.edu>
2018-07-31 09:32:37 -0400

    now uses exchange, data cleaning wip

--- read-email.py
+++ read-email.py
@@ -1,52 +1,54 @@
 # dependencies
 from imapclient import IMAPClient
-import email
-import secrets
-import os
-import csv
-import re
+import email, secrets, os, csv, re, datetime, re
+from datetime import timedelta
+import pandas as pd
 
-parsedData = []
-tweets = []
+yesterday = datetime.datetime.today() - timedelta(days=1)
+dispatch_data = []
 
 def login_and_write():
-    # connects to IMAP server 
-    server = IMAPClient('imap.gmail.com', use_uid=True)
-    server.login(secrets.email, secrets.pwd)
+    # Open csv
+    info_sheet = open('fire-dept-twitter.csv', 'w') 
+    # if there's no header, write a header
+    if os.stat('fire-dept-twitter.csv').st_size == 0:
+        info_sheet.write("CAD, Address, City, Type of Incident, ID, ID2, Date \n") 
+
+    # Connect to imap server
+    server = IMAPClient(host='email.townofchapelhill.org',port=143,use_uid=True,ssl=False)
+    server.starttls()
+    print("Connecting...")
+    try: 
+        server.login(secrets.odsuser,secrets.odspass)
+        print("Connected.") 
+    except: 
+        print("Not connected.")
     b'[CAPABILITY IMAP4rev1 LITERAL+ SASL-IR [...] LIST-STATUS QUOTA] Logged in'
 
     # Selects inbox as target
-    select_info = server.select_folder('INBOX')
+    server.select_folder('INBOX')
+    # Searches inbox for emails containing chfd
+    messages = server.search(['TEXT', 'CHFD'])    
 
-    # Searches inbox for emails from desired address
-    messages = server.search(['FROM', secrets.target_email])
-
-    # opens csv
-    info_sheet = open('fire-dept-twitter.csv', 'w')    
-
-    # if there's no header, write a header
-    if os.stat('fire-dept-twitter.csv').st_size == 0:
-        info_sheet.write("Address, City, Type of Incident \n") 
-
+    datetimes = []
+    for msgid, datedata in server.fetch(messages, ['ENVELOPE']).items():
+        dateenvelope = datedata[b'ENVELOPE']
+        for item in dateenvelope:
+            if isinstance(item, datetime.datetime):
+                datetimes.append(item)
+    
     # loops through messages to fetch text content
     # reassigns the parsedData list each iteration to prevent duplicates
     for msgid, data in server.fetch(messages, ['BODY[TEXT]']).items():
         envelope = data[b'BODY[TEXT]']
-        target = str(envelope.decode()[14:])
-        parsedData = target.split(";")
-        parsedData.pop(0)
-        # The emails typically contain special characters
-        # using regex to remove them
-        try:
-            new_string1 = re.sub('[^A-Za-z0-9 ]+', '', parsedData[0])
-            new_string2 = re.sub('[^A-Za-z0-9 ]+', '', parsedData[1])
-            new_string3 = re.sub('[^A-Za-z0-9 ]+', '', parsedData[2])
-            info_sheet.write(new_string1 + ",")
-            info_sheet.write(new_string2 + ",")
-            info_sheet.write(new_string3 + "\n")
-        except:
-            continue
-
+        target = envelope.decode()
+        dispatch_data = re.findall("((CAD:).*)\n",target)
+        x = 0 
+        for item in dispatch_data:
+            while x < len(dispatch_data):
+                info_sheet.write(str(item[0]).replace(";", ", ").rstrip() + ", " + str(datetimes[x]) + "\n")
+                x = x + 1
+    info_sheet.close()
     logout(server)
 
 # Logs out of remote session
